b3024172e20f954adc96974973c60d496aa2ecba
Iamthecoderofgods <22mjames@strichardgwyn.school>
2024-10-15 18:48:57 +0100

    Add files via upload

---
+++ my.py
@@ -0,0 +1,12 @@
+capital ={"india":"Delhi", "France":"Paris","srilanka":"colombo"}
+print(capital)
+
+print(capital["india"])
+
+print(len(capital))
+
+dict1={"brand":"ford", "electric":False, "year": 1963, "colors":["red","white","blue"]}
+print(type(dict1))
+
+b = [1,2,3,4]
+print(type(b))
